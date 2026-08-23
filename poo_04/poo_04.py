9581b8abde0d4bc17378fb98ef900a34bad92c8f
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-22 23:19:58 -0300

    commit_01.py

--- poo_04/poo_04.py
+++ poo_04/poo_04.py
@@ -1,1 +1,10 @@
+# - Exercício de Fixação 01 - Questão 1 -
 
+numero = int(input("Digite um número inteiro: "))
+
+fatorial = 1
+
+for i in range(1, numero + 1):
+    fatorial = fatorial * i
+
+print("O fatorial de", numero, "é:", fatorial)
