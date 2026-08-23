d486395b4d7501e026db498b35137f29fffeb1bf
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-22 23:10:57 -0300

    commit_03.py

--- poo_03/poo_03.py
+++ poo_03/poo_03.py
@@ -16,3 +16,16 @@ print("Média:", media)
 
 if media >= 30 and media < 70:
     print("Você está em RECUPERAÇÃO!!!")
+
+# - Exercício de Fixação 03 -
+
+inicio = int(input("Digite o início do intervalo: "))
+fim = int(input("Digite o fim do intervalo: "))
+valor = int(input("Digite o valor a ser verificado: "))
+
+if valor >= inicio and valor <= fim:
+    print("O valor está dentro do intervalo.")
+elif valor < inicio:
+    print("O valor está abaixo do intervalo.")
+else:
+    print("O valor está acima do intervalo.")
