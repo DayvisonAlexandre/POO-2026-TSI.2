cc9e5d29147ea4289f3eb5dac0a192858c392832
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-22 23:37:59 -0300

    commit_01.py

--- poo_06/poo_06.py
+++ poo_06/poo_06.py
@@ -1,1 +1,23 @@
+# - Exercício de Fixação 01 - Tuplas -
 
+def elementos_pares(tupla):
+    pares = tuple(numero for numero in tupla if numero % 2 == 0)
+    return pares
+
+
+numeros = (1, 2, 3, 4, 5)
+
+resultado = elementos_pares(numeros)
+
+print(resultado)
+
+def ordenar_sem_repeticoes(tupla):
+    resultado = tuple(sorted(set(tupla)))
+    return resultado
+
+
+nomes = ("banana", "maçã", "laranja", "banana", "uva")
+
+resultado = ordenar_sem_repeticoes(nomes)
+
+print(resultado)
