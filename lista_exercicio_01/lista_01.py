106c3445e0dd18454a60c30e5cf97ef7e11af4ba
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-23 00:16:40 -0300

    commit_01.py

--- lista_exercicio_01/lista_01.py
+++ lista_exercicio_01/lista_01.py
@@ -1,1 +1,16 @@
+# - Questão - 01 -
 
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print("\nNotas digitadas:")
+print(nota1)
+print(nota2)
+print(nota3)
+print(nota4)
+
+print(f"Média aritmética: {media:.2f}")
