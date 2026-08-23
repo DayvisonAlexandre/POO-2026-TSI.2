77bee55a58cd37fe05c53f2d333963619259c08e
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-22 23:02:13 -0300

    commit_01.py

--- poo_01/poo_01.py
+++ poo_01/poo_01.py
@@ -1,1 +1,16 @@
+# - Exercício de fixação 01 - 
 
+# Solicita dois valores inteiros
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+
+# Exibe os resultados
+print("A + B =", A + B)
+print("A * B =", A * B)
+print("A - B =", A - B)
+
+# Verifica se B é diferente de zero antes da divisão
+if B != 0:
+    print("A / B =", A / B)
+else:
+    print("A / B = Não é possível dividir por zero")
