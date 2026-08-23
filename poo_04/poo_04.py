5ead3ad6fbed1d7192521ae957c4b3685d6738c1
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-22 23:21:58 -0300

    commit_02.py

--- poo_04/poo_04.py
+++ poo_04/poo_04.py
@@ -8,3 +8,42 @@ for i in range(1, numero + 1):
     fatorial = fatorial * i
 
 print("O fatorial de", numero, "é:", fatorial)
+
+# - Exercício de Fixação 01 - Questão 2 -
+
+quantidade = int(input("Quantos usuários irão responder? "))
+
+while quantidade <= 0:
+    print("A quantidade deve ser maior que zero.")
+    quantidade = int(input("Quantos usuários irão responder? "))
+
+insatisfeito = 0
+satisfeito = 0
+nao_responder = 0
+
+for i in range(quantidade):
+    print("\n1 - INSATISFEITO")
+    print("2 - SATISFEITO")
+    print("3 - NÃO QUERO RESPONDER")
+
+    resposta = int(input("Digite a opção: "))
+
+    while resposta < 1 or resposta > 3:
+        print("Opção inválida!")
+        resposta = int(input("Digite a opção novamente: "))
+
+    if resposta == 1:
+        insatisfeito += 1
+    elif resposta == 2:
+        satisfeito += 1
+    else:
+        nao_responder += 1
+
+percentual_insatisfeito = insatisfeito * 100 / quantidade
+percentual_satisfeito = satisfeito * 100 / quantidade
+percentual_nao_responder = nao_responder * 100 / quantidade
+
+print("\n=== RESULTADO DA PESQUISA ===")
+print("INSATISFEITO:", percentual_insatisfeito, "%")
+print("SATISFEITO:", percentual_satisfeito, "%")
+print("NÃO QUERO RESPONDER:", percentual_nao_responder, "%")
