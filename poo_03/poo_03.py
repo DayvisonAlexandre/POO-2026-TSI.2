7ffed5cfe8c67263e7cc733a9a2c8259c5d49f20
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-22 23:13:42 -0300

    commit_02.py

--- poo_03/poo_03.py
+++ poo_03/poo_03.py
@@ -5,6 +5,18 @@ numero = int(input("Digite um número inteiro: "))
 if numero > 0:
     print("O número é positivo!")
 
+# - Exercício de Fixação 02 -
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print("Média:", media)
+
+if media >= 30 and media < 70:
+    print("Você está em RECUPERAÇÃO!!!")
+
 # - Exercício de Fixação 03 -
 
 inicio = int(input("Digite o início do intervalo: "))
