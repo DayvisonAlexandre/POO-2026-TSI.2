a1043b0142872479e5933038142f9ab446870f49
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-22 23:15:57 -0300

    commit_04.py

--- poo_03/poo_03.py
+++ poo_03/poo_03.py
@@ -29,3 +29,39 @@ elif valor < inicio:
     print("O valor está abaixo do intervalo.")
 else:
     print("O valor está acima do intervalo.")
+
+# - Exercício de Fixação 04 -
+
+print("=== MENOR CAMINHO ===")
+
+ab = float(input("Digite a distância entre A e B: "))
+ac = float(input("Digite a distância entre A e C: "))
+
+bd = float(input("Digite a distância entre B e D: "))
+be = float(input("Digite a distância entre B e E: "))
+
+cf = float(input("Digite a distância entre C e F: "))
+cg = float(input("Digite a distância entre C e G: "))
+
+# Calcula as quatro possibilidades de caminho
+caminho1 = ab + bd
+caminho2 = ab + be
+caminho3 = ac + cf
+caminho4 = ac + cg
+
+# Verifica qual é o menor caminho
+if caminho1 <= caminho2 and caminho1 <= caminho3 and caminho1 <= caminho4:
+    print("Caminho percorrido: A → B → D")
+    print("Distância percorrida:", caminho1)
+
+elif caminho2 <= caminho1 and caminho2 <= caminho3 and caminho2 <= caminho4:
+    print("Caminho percorrido: A → B → E")
+    print("Distância percorrida:", caminho2)
+
+elif caminho3 <= caminho1 and caminho3 <= caminho2 and caminho3 <= caminho4:
+    print("Caminho percorrido: A → C → F")
+    print("Distância percorrida:", caminho3)
+
+else:
+    print("Caminho percorrido: A → C → G")
+    print("Distância percorrida:", caminho4)
