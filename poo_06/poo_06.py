f5f21297abc21bb591e42e7b922f6a0add5c17e3
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-22 23:39:28 -0300

    commit_02.py

--- poo_06/poo_06.py
+++ poo_06/poo_06.py
@@ -21,3 +21,46 @@ nomes = ("banana", "maçã", "laranja", "banana", "uva")
 resultado = ordenar_sem_repeticoes(nomes)
 
 print(resultado)
+
+# - Exercício de Fixação 02 - Dicionários -
+
+def pais_maior_populacao(paises):
+    if not paises:
+        return None
+
+    maior_pais = max(paises, key=paises.get)
+
+    return maior_pais
+
+
+populacoes = {
+    "Brasil": 211.8,
+    "China": 1400.5,
+    "Índia": 1366.4
+}
+
+resultado = pais_maior_populacao(populacoes)
+
+print(resultado)
+
+def alunos_aprovados(alunos):
+    aprovados = {}
+
+    for nome, notas in alunos.items():
+        media = sum(notas) / len(notas)
+
+        if media >= 7:
+            aprovados[nome] = round(media, 2)
+
+    return aprovados
+
+
+alunos = {
+    "Ana": [8.5, 9.0, 7.5],
+    "Bruno": [6.0, 5.5, 4.0],
+    "Carla": [7.0, 8.0, 9.0]
+}
+
+resultado = alunos_aprovados(alunos)
+
+print(resultado)
