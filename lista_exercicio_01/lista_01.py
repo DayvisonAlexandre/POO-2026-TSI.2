64ac447a7b6bed79e77063040074a7f800bcb8be
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-24 20:21:24 -0300

    commit_12.py

--- lista_exercicio_01/lista_01.py
+++ lista_exercicio_01/lista_01.py
@@ -137,3 +137,15 @@ else:
     diferenca = valor2 - valor1
 
 print(f"Diferença: {diferenca}")
+
+# - Questão - 12 -
+
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+valor3 = int(input("Digite o terceiro valor: "))
+
+valores = [valor1, valor2, valor3]
+valores.sort()
+
+print("Valores em ordem crescente:")
+print(valores[0], valores[1], valores[2])
