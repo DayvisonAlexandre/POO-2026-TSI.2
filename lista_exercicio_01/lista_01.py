2c9a03c32d8447ef21a0b578195f334e4566b04e
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-08-24 20:27:45 -0300

    commit_19.py

--- lista_exercicio_01/lista_01.py
+++ lista_exercicio_01/lista_01.py
@@ -220,3 +220,15 @@ for i in range(5):
         soma += numero
 
 print(f"Somatório dos valores menores que 10: {soma}")
+
+# - Questão - 19 -
+
+soma = 0
+
+for i in range(5):
+    numero = float(input(f"Digite o {i + 1}º valor: "))
+
+    if numero >= 10 and numero < 20:
+        soma += numero
+
+print(f"Somatório: {soma}")
