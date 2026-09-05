b03d68b956d501afce926a0827ce4ebf2e7c449f
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-09-05 15:53:03 -0300

    commit_pessoa.py

--- poo_09/poo_09.py
+++ poo_09/poo_09.py
@@ -1,1 +1,18 @@
-print ("hello world")
+class Pessoa:
+    def __init__(self, nome, idade=20, cep=None):
+        self.nome = nome
+        self._idade = idade
+        self.__cep = cep
+
+    def dizerNome(self):
+        print(f"Meu nome é {self.nome}")
+
+    def atualizarCEP(self, cep):
+        if len(cep) != 8:
+            print("Quantidade de dígitos inválida!")
+        else:
+            self.__cep = cep
+            print("CEP atualizado com sucesso!")
+
+    def isAdulto(self) -> bool:
+        return True if self._idade >= 18 else False
