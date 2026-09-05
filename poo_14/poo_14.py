e7fb35972e8db35c98992f6fa5a42aca30bd2710
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-09-05 16:55:55 -0300

    commit_programa.py

--- poo_14/poo_14.py
+++ poo_14/poo_14.py
@@ -80,3 +80,37 @@ class Registro:
         print("\n----------------------------------------------")
         print(f"TOTAL GERAL RECEBIDO: R$ {total_geral:.2f}")
         print("==============================================")
+
+
+from registro import Registro
+from item_venda import ItemVenda
+from servico import Servico
+
+
+def main():
+
+    registro = Registro()
+
+    # Criando itens de venda
+    item1 = ItemVenda("Memória RAM 8GB", 2, 150.00)
+    item2 = ItemVenda("SSD 480GB", 1, 280.00)
+    item3 = ItemVenda("Teclado USB", 2, 75.00)
+
+    # Criando serviços
+    servico1 = Servico("Manutenção preventiva", 2.0, 80.00)
+    servico2 = Servico("Formatação de computador", 1.5, 100.00)
+
+    # Adicionando os recebimentos ao registro
+    registro.adicionarItem(item1)
+    registro.adicionarItem(item2)
+    registro.adicionarItem(item3)
+
+    registro.adicionarItem(servico1)
+    registro.adicionarItem(servico2)
+
+    # Listando todos os recebimentos
+    registro.listarItem()
+
+
+if __name__ == "__main__":
+    main()
