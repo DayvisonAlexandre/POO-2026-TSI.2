48a6850ba1ea7cf7c5065172ac8dee38583a9ff0
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-09-05 15:39:19 -0300

    commit_classe_rectangle.py

--- poo_08/poo_08.py
+++ poo_08/poo_08.py
@@ -9,3 +9,26 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+
+# ============================================================
+# CLASSE RECTANGLE
+# ============================================================
+
+class Rectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def corners(self):
+        """
+        Retorna os quatro cantos do retângulo.
+        """
+        return [
+            Point(self.x, self.y),
+            Point(self.x + self.width, self.y),
+            Point(self.x, self.y + self.height),
+            Point(self.x + self.width, self.y + self.height)
+        ]
