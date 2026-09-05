0c94c6cbf5fed02abc20433ffd533fb51d71d931
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-09-05 15:41:12 -0300

    commit_01.py

--- poo_08/poo_08.py
+++ poo_08/poo_08.py
@@ -32,3 +32,24 @@ class Rectangle:
             Point(self.x, self.y + self.height),
             Point(self.x + self.width, self.y + self.height)
         ]
+
+
+# - Questão 01 -
+
+
+class Circle:
+    def __init__(self, center, radius):
+        self.center = center
+        self.radius = radius
+
+
+print("=" * 60)
+print("QUESTÃO 1 - DEFINIÇÃO DA CLASSE CIRCLE")
+print("=" * 60)
+
+centro_teste = Point(0, 0)
+circulo_teste = Circle(centro_teste, 10)
+
+print("Classe Circle criada com sucesso!")
+print(f"Centro: ({circulo_teste.center.x}, {circulo_teste.center.y})")
+print(f"Raio: {circulo_teste.radius}")
