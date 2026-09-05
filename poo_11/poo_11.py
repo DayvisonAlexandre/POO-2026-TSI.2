4ddc277ba335ec79f54fdc38dcbc3e468e217adc
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-09-05 16:27:34 -0300

    commit_cachorro.py

--- poo_11/poo_11.py
+++ poo_11/poo_11.py
@@ -6,3 +6,24 @@ class Animal:
 
     def moverse(self):
         pass
+
+
+from animal import Animal
+
+
+class Cachorro(Animal):
+    def __init__(self, nome, peso):
+        super().__init__(nome, peso)
+        self._qtdSaltos = 0
+
+    def latir(self):
+        print("Au Au!")
+
+    def saltar(self):
+        self._qtdSaltos += 1
+
+    def descansar(self):
+        self._qtdSaltos -= 1
+
+    def estaCansado(self):
+        return self._qtdSaltos > 5
