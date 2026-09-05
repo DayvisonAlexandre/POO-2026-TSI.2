60b1f91fb338b3574e89a92c57b0a6185d9a6e29
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-09-05 16:25:59 -0300

    commit_animal.py

--- poo_11/poo_11.py
+++ poo_11/poo_11.py
@@ -1,1 +1,8 @@
-print ("hello world")
+class Animal:
+    def __init__(self, nome, peso):
+        self._nome = nome
+        self._peso = peso
+        self._posicao = 0
+
+    def moverse(self):
+        pass
