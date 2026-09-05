ee94051b36dd524a38656777c037cf310f0d9138
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-09-05 16:51:38 -0300

    commit_servico.py

--- poo_14/poo_14.py
+++ poo_14/poo_14.py
@@ -28,3 +28,25 @@ class ItemVenda(IRecebivel):
             f"Valor unitário: R$ {self.valor:.2f} | "
             f"Total: R$ {self.totalizarRecebivel():.2f}"
         )
+
+
+from irecebivel import IRecebivel
+
+
+class Servico(IRecebivel):
+
+    def __init__(self, descricao: str, horas_servico: float, valor_hora: float):
+        self.descricao = descricao
+        self.horas_servico = horas_servico
+        self.valor_hora = valor_hora
+
+    def totalizarRecebivel(self) -> float:
+        return self.horas_servico * self.valor_hora
+
+    def __str__(self) -> str:
+        return (
+            f"Serviço: {self.descricao} | "
+            f"Horas: {self.horas_servico:.2f} | "
+            f"Valor por hora: R$ {self.valor_hora:.2f} | "
+            f"Total: R$ {self.totalizarRecebivel():.2f}"
+        )
