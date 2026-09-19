aa5502ecdf534029e75e5834c2e616bb9ce54f59
DayvisonAlexandre <dayvison.alexandre@escolar.ifrn.edu.br>
2026-09-18 23:05:10 -0300

    commit_01.py

--- poo_11_encapsulamento_01/poo_11_encapsulamento_01.py
+++ poo_11_encapsulamento_01/poo_11_encapsulamento_01.py
@@ -1,1 +1,29 @@
-print ("Hello World")
+# - Exercício 01 — ContaBancaria -
+
+class ContaBancaria:
+    def __init__(self, titular: str) -> None:
+        self.titular = titular
+        self._saldo = 0.0
+
+    def depositar(self, valor: float) -> None:
+        self._saldo += valor
+
+    def sacar(self, valor: float) -> bool:
+        if valor > self._saldo:
+            return False
+
+        self._saldo -= valor
+        return True
+
+    def saldo(self) -> float:
+        return self._saldo
+
+
+conta = ContaBancaria("Maria")
+conta.depositar(100.0)
+
+print(conta.saldo())
+print(conta.sacar(50.0))
+print(conta.saldo())
+print(conta.sacar(100.0))
+print(conta.saldo())
